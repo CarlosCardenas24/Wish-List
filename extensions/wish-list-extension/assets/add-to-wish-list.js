7c9b9a917029f20cce2716badc7ff213a3372de0
CarlosCardenas24 <nooptions101@gmail.com>
2023-11-06 19:35:51 -0800

    can't POST through liquid, so passing data to a different url

--- extensions/wish-list-extension/assets/add-to-wish-list.js
+++ extensions/wish-list-extension/assets/add-to-wish-list.js
@@ -1,5 +1,30 @@
-var javaScript = document.getElementById('myScript')
+/* //import prisma from "~/db.server.js"
+/* var javaScript = document.getElementById('myScript')
 
 let wish_counter = javaScript.getAttribute('wish_counter')
 let productID = javaScript.getAttribute('productID')
 let productTitle = javaScript.getAttribute('productTitle')
+
+function counter () {
+    wish_counter++
+    console.log(wish_counter)
+  }
+
+const action = async ({request}) => {
+    const form = await request.formData()
+    const fields = {productID, productTitle}
+
+    const wishList = await prisma.wishList.create({data: fields})
+    
+    return null
+}
+
+document.getElementById("addToWishListButton").addEventListener("click", action) */
+var javaScript = document.getElementById('myScript')
+let productTitle = javaScript.getAttribute('productTitle')
+
+function redirectToExample() {
+  window.location.href = `https://admin.shopify.com/store/wish-list-alpha/apps/wish-list-18/app/config/${productTitle}`;
+}
+
+document.getElementById("addToWishListButton").addEventListener("click", redirectToExample)
