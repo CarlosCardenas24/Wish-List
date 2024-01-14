b2beb6482c225f5ae85b8b131741d3c4ca5fbc48
CarlosCardenas24 <nooptions101@gmail.com>
2024-01-13 21:24:01 -0800

    trying to use useSubmit to save data to the db

--- app/routes/app.add.jsx
+++ app/routes/app.add.jsx
@@ -33,27 +33,37 @@ export async function loader({ request, params }) {
     const productData = [product.data]
     return {productData, productId}
 
-  
+   
 }
- 
-// wrap loader method inside another function, call the function on initial function (use effect, [])
 
-  export default function Index() {
+
+
+export default function Index() {
     const {productData} = useLoaderData()
     const {productId} = useLoaderData()
 
     const sameProduct = productData.map((item) => {
-
         for (let i = 0; i < item.length; i++) {
             let newId = item[i].id
-            let newTitle = item[i].title
+            let productTitle = item[i].title
             if(newId == productId) {
-                return [newId, newTitle]
+                return {productId, productTitle}
             }
-         }
+        }
     })
 
-    console.log(sameProduct)
+    // Save function 
+
+    // set up event
+    submitData () => {
+        const submit = useSubmit()
+        return (
+            <Form onChange={(event) => {
+                submit(event.currentTarget)
+            }}
+            />
+        )
+    }
 
     return (
     <Page>
