218152099fcec98cf5d0370803227e5a05662531
CarlosCardenas24 <nooptions101@gmail.com>
2024-02-17 20:13:57 -0800

    removed old code

--- app/routes/api.product.storage.jsx
+++ app/routes/api.product.storage.jsx
@@ -1,18 +1,25 @@
+//@Documentation -> https://www.prisma.io/docs/orm/prisma-client/queries/crud
 import { PrismaClient } from "@prisma/client";
 import { json } from "@remix-run/node";
-import fs from 'fs/promises';
 
 const prisma = new PrismaClient();
 prisma ? console.timeStamp() : console.error({ message: "Prisma ORM failed to initialize"});
 
+const accessOptions = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type"
+};
+
+const successMessage = {
+    message: "Success! Product Added to Database",
+    method: 'action()',
+};
+
 export async function loader({ request }) {
     try {
         console.log("request", request);
-        let headers = new Headers({
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
-            "Access-Control-Allow-Headers": "Content-Type"
-        });
+        let headers = new Headers(accessOptions);
         return json({ message: "Success!", method: 'loader()' }, { headers });
     } catch (error) {
         console.log(error);
@@ -20,50 +27,45 @@ export async function loader({ request }) {
 
 }
 
-/*@NOTES grab id from action method and generate a storage JSON file from it to simulate database storage */
-
 export async function action({ request }) {
-
-    const accessOptions = {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type"
-    };
-
-    const successMessage = {
-        message: "Success! Product Added to Database",
-        method: 'action()',
-    };
-
-    const structure = id => {
-       return { product_id : id }
-    }
-
-    const path = './db-related-files/product.json';
-    
     try {
-
         let headers = new Headers(accessOptions);
         let body = await request.json();
         const { productId } = body;
 
-        await prisma.product.create({
-            data: {
-                id: productId,
-                title: "Test Product",
+        const recordExists = await prisma.product.findUnique({
+            where: {
+                id: productId
             }
         });
 
-        // const products = await prisma.product.findMany();
-        // console.log("products", products)
+        if(recordExists){
+            const { quantity } = recordExists;
 
-        const products = await prisma.product.findMany();
+            await prisma.product.update({
+                where: {
+                    id: productId
+                },
+                data: {
+                    quantity: quantity + 1
+                },
+            })
 
-        console.log("products", products)
+        } else {
+            await prisma.product.create({
+                data: {
+                    id: productId,
+                    name: "Product Name",
+                    quantity: 1,
+                }
+            });
 
-        return json({ headers });
+        }
 
-    } catch (error) {
-        console.log(error);
-    }
+        return json({ successMessage }, { headers });
+
+        } catch (error) {
+            console.log(error);
+        }
+    return json({ successMessage });
 }
