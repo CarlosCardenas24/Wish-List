cf9f0432e9709c29ec0d37c909d4020d9bdd6706
Alvaro Castro Cid <alvarocastrocid@Alvaros-MacBook-Pro.local>
2024-03-05 20:08:49 -0500

    added more robust backend;

--- extensions/wish-list-extension/assets/script.js
+++ extensions/wish-list-extension/assets/script.js
@@ -1,31 +1,33 @@
+const titleInput = document.querySelector('.title');
+const priceInput = document.querySelector('.price');
+const shopInput = document.querySelector('.shop-id');
+const productInput = document.querySelector('.product-id');
 const button = document.querySelector('.add-to-list-button');
-let productId = null;
-// needed for shop id
-var javaScript = document.getElementById('myScript')
-let shopId = javaScript.getAttribute('shopId')
 
-if(!button) {
-    console.error('Button not found: ' + button);
-}
-if(!shopId) {
-    console.error('Shop Id not found: ' + shop_Id);
-}
-
-console.log(shopId)
+const title = titleInput?.value;
+const price = priceInput?.value;
+const shopId = shopInput?.value;
+const productId = productInput?.value;
 
 button?.addEventListener('click', () => {
-    productId = button.getAttribute('id');
-
     const PORT = 3000;
+
+    const values = [title, price, shopId, productId];
+
+    if(values.some(value => !value)){
+        console.log('Please fill out all fields');
+        return null;
+    }
 
     fetch(`http://localhost:${PORT}/api/product/storage`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ productId, shopId })
+        body: JSON.stringify({ productId, shopId, price, title })
     })
     .then(response => response.json())
     .then(data => { console.log(data)})
     .catch(error => console.error(error));
+
 });
