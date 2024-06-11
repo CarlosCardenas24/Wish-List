94efbb340a841c5a054ed569ebc0718e57c087c7
Alvaro Castro Cid <alvarocastrocid@Alvaros-MacBook-Pro.local>
2024-06-11 19:06:42 -0400

    enabled rough pop ux for user wishlist;

--- app/routes/api.product.storage.jsx
+++ app/routes/api.product.storage.jsx
@@ -15,7 +15,93 @@ const accessOptions = {
 const successMessage = {
     message: "Success! Product Added to Database",
     method: 'action()',
+    resource: {},
+    status: 200,
 };
+
+async function updateProduct(record, variantId) {
+    const { quantity } = record;
+    const _quantity = {
+        where: {
+            variantId: variantId
+        }, 
+        data: {
+            quantity: quantity + 1,
+        }
+    }
+
+    return prisma.product.update(_quantity)
+}
+
+async function addProduct(body) {
+    const { productId, title, shopId, variantId, variantTitle, price, image } = body;
+
+    const _product = {
+        data : {
+            id: productId,
+            name: title,
+            shopId: shopId,
+            variantId: variantId,
+            variantName: variantTitle,
+            price: price,
+            image: image,
+            quantity: 1,
+        }
+    }
+
+    return prisma.product.create(_product);
+}
+
+async function updateUserWishList(body) {
+    const { userId, variantId } = body;
+
+    const existingList = {
+        wishList: {
+            upsert: {
+                create: {
+                    quantity: 1,
+                    variantId: variantId,
+                },
+                update: {
+                    quantity: { increment: 1 },
+                },
+                where: {
+                    variantId_uId: {variantId: variantId, uId: userId}
+                }
+            },
+        }
+    }
+
+    const _wishList = {
+        where: { userId: userId },
+        data: existingList,
+        include: { wishList: true }
+    }
+
+    const update = await prisma.user.update(_wishList);
+
+    successMessage.resource = update;
+
+    return update;
+}
+
+async function createWishList(body) {
+    const { userId, variantId } = body;
+
+    const _newList = {
+        data: {
+            userId: userId,
+            wishList: {
+                create: {
+                    quantity: 1,
+                    variantId: variantId,
+                }
+            }
+        }
+    }
+
+    return prisma.user.create(_newList);
+}
 
 export async function loader({ request }) {
     try {
@@ -28,94 +114,39 @@ export async function loader({ request }) {
 }
 
 export async function action({ request }) {
+    
+    console.log('request from action(): ', request)
+    
     try {
         let headers = new Headers(accessOptions);
         let body = await request.json();
-        const { userId, productId, title, shopId, variantId, variantTitle, price, image } = body;
+        const { userId, variantId } = body;
 
-        const recordExists = await prisma.product.findUnique({
+        //query for a product with matching variantId
+        const _variantId = {
             where: {
                 variantId: variantId
             }
-        });
-
-        const userExists = await prisma.user.findUnique({
+        }
+        
+        //query for a user with matching userId
+        const _userId = {
             where: {
                 userId: userId
             }
-        })
-
-        if (recordExists) {
-            const { quantity } = recordExists;
-
-            await prisma.product.update({
-                where: {
-                    variantId: variantId
-                },
-                data: {
-                    quantity: quantity + 1
-                },
-            })
-
-        } else {
-            await prisma.product.create({
-                data: {
-                    id: productId,
-                    name: title,
-                    shopId: shopId,
-                    variantId: variantId,
-                    variantName: variantTitle,
-                    price: price,
-                    image: image,
-                    quantity: 1,
-                }
-            });
         }
-        if (userExists) {
-            await prisma.user.update({
-                where: {
-                    userId: userId
-                },
-                data: {
-                    wishList: {
-                        upsert: {
-                            create: {
-                                quantity: 1,
-                                variantId: variantId,
-                            },
-                            update: {
-                                quantity: { increment: 1 },
-                            },
-                            where: {
-                                variantId_uId: {variantId: variantId, uId: userId}
-                            }
-                        },
-                        
-                    }
-                },
-                include: {
-                    wishList: true,
-                },
-            })
-        } else {
-            await prisma.user.create({
-                data: {
-                    userId: userId,
-                    wishList: {
-                        create: {
-                            quantity: 1,
-                            variantId: variantId,
-                        }
-                    },
-                }
-            });
-        }
+
+        const recordExists = await prisma.product.findUnique(_variantId);
+        const userExists = await prisma.user.findUnique(_userId)
+        const record = recordExists;
+        recordExists ? await updateProduct(record, variantId) : await addProduct(body);
+        userExists ? await updateUserWishList(body) : await createWishList(body);
 
         return json({ successMessage }, { headers });
 
     } catch (error) {
         console.log(error);
-        return json({ message: "hello!" });
+        return json({ message: "Hello, the request to the API has failed!" });
     }
 
 }
