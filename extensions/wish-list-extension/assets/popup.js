dc78310a4220d26f435d906f6bb15cc720272b8e
CarlosCardenas24 <nooptions101@gmail.com>
2024-06-25 15:30:34 -0700

    finished styling mobile view of the popup when adding something to the wish list and made the X and Continue Shopping buttons functional

---
+++ extensions/wish-list-extension/assets/popup.js
@@ -0,0 +1,4 @@
+function closePopup() {
+    document.getElementById('popup-container-success').style.display = "none";
+    document.getElementById('popup-container-error').style.display = "none";
+}
