abf61bddff96cafa68104169dc4bd0ab6d2d79f1
CarlosCardenas24 <nooptions101@gmail.com>
2024-09-04 15:06:01 -0700

    testting weebhook for removing customers from db for a specific store

--- app/routes/webhooks.jsx
+++ app/routes/webhooks.jsx
@@ -2,7 +2,7 @@ import { authenticate } from "../shopify.server";
 import prisma from "../db.server";
 
 export const action = async ({ request }) => {
-  const { topic, shop, session } = await authenticate.webhook(request);
+  const { topic, shop, session, payload } = await authenticate.webhook(request);
 
   switch (topic) {
     case "APP_UNINSTALLED":
@@ -11,7 +11,13 @@ export const action = async ({ request }) => {
       }
       break;
     //case "CUSTOMERS_DATA_REQUEST":
-    //case "CUSTOMERS_REDACT":
+      
+      //break;
+    case "CUSTOMERS_REDACT":
+        const customer = payload.customer;
+        const customerId = customer.id
+        await prisma.user.delete({where : { userId_shopId: {userId: customerId, shopId: payload.shop_id} }})
+    break;
     //case "SHOP_REDACT":
     default:
       throw new Response("Unhandled webhook topic", { status: 404 });
