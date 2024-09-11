3a7719df4542205bdfbc27d92b493b10e852cbc8
CarlosCardenas24 <nooptions101@gmail.com>
2024-09-11 16:09:47 -0700

    changed the code a bit in webhooks with the if statement for customer redact

--- app/routes/webhooks.jsx
+++ app/routes/webhooks.jsx
@@ -10,15 +10,21 @@ export const action = async ({ request }) => {
         await prisma.session.deleteMany({ where: { shop } });
       }
       break;
-    //case "CUSTOMERS_DATA_REQUEST":
+    case "CUSTOMERS_DATA_REQUEST":
       
-      //break;
+      break;
     case "CUSTOMERS_REDACT":
-        const customer = payload.customer;
-        const customerId = customer.id;
-        await prisma.user.delete({where : { userId_shopId: {userId: customerId, shopId: payload.shop_id} }});
+      const {customer} = payload;
+      const {shop_id} = payload;
+      if (customer && shop_id ) {
+        await prisma.user.delete({where : { userId_shopId: {userId: customer.id, shopId: payload.shop_id} }});
+      }
     break;
-    //case "SHOP_REDACT":
+    case "SHOP_REDACT":
+      const {shop_id} = payload;
+      await prisma.product.deleteMany({ where: { shopId: {shop_id} } })
+      await prisma.product.deleteMany({ where: { shopId: {shop_id} } })
+    break;
     default:
       throw new Response("Unhandled webhook topic", { status: 404 });
   }
