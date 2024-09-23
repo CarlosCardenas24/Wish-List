aa50f5169b433d75b4d2bf5e5d46cabf55f0034f
CarlosCardenas24 <nooptions101@gmail.com>
2024-09-23 12:14:10 -0700

    added some console.logs and changed the code a bit for customers_redact

--- app/routes/webhooks.jsx
+++ app/routes/webhooks.jsx
@@ -2,9 +2,11 @@ import { authenticate } from "../shopify.server";
 import prisma from "../db.server";
 
 export const action = async ({ request }) => {
-  console.log(request)
   const { topic, shop, session, payload } = await authenticate.webhook(request);
   console.log("Payload: ", payload)
+
+  let userExists;
+  let shopExists;
 
   switch (topic) {
     case "APP_UNINSTALLED":
@@ -16,22 +18,28 @@ export const action = async ({ request }) => {
       
       break;
     case "CUSTOMERS_REDACT":
-      const {customer} = payload;
-      const idShop = payload.shop_id;
+      const {customer, shop_id} = payload;
       const customerString = '' + customer.id
-      const idShopString = '' + idShop
+      const shopString = '' + shop_id
+      console.log("Id: ", customerString)
+      console.log("Shop: ", shopString)
 
-      const userExists = await prisma.user.findUniqueOrThrow({where : { userId_shopId: {userId: customerString, shopId: idShopString} }})
-      if (userExists ) {
-        console.log("id: ", customerString)
-        console.log("shop: ", idShopString)
-        await prisma.user.delete({where : { userId_shopId: {userId: customerString, shopId: idShopString} }});
+      userExists = await prisma.user.findUniqueOrThrow({where : { userId_shopId: {userId: customerString, shopId: shopString} }})
+      if ( userExists ) {
+        await prisma.user.delete({where : { userId_shopId: {userId: customerString, shopId: shopString} }});
       }
     break;
     case "SHOP_REDACT":
       const shopId = payload.shop_id;
-      await prisma.product.deleteMany({ where: { shopId: {shopId} } })
-      await prisma.product.deleteMany({ where: { shopId: {shopId} } })
+      const idShopString = '' + shopId
+
+      shopExists = await prisma.user.findUniqueOrThrow({where : { shopId: idShopString } })
+      userExists = await prisma.user.findUniqueOrThrow({where : { shopId: idShopString }})
+
+      if ( shopExists && userExists ) {
+        await prisma.product.deleteMany({ where: { shopId: idShopString } })
+        await prisma.user.deleteMany({ where: { shopId: idShopString } })
+      }
     break;
     default:
       throw new Response("Unhandled webhook topic", { status: 404 });
