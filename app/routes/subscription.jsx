ddc03fc880aad7d902ce59545dc55870692ad34e
CarlosCardenas24 <nooptions101@gmail.com>
2024-10-16 16:34:03 -0700

    added async function to subscription.jsx

--- app/routes/subscription.jsx
+++ app/routes/subscription.jsx
@@ -1,42 +1,47 @@
-const { session, admin } = await authenticate.admin(request);
+async function subscription() {
+    const { session, admin } = await authenticate.admin(request);
 
-const shop = session.shop.replace(".myshopify.com", "");
-const appHandle = "wishifylist"
-const returnUrl = `https://vital-wish-list-c86fce13cbd9.herokuapp.com/charge_approval`;
+    const shop = session.shop.replace(".myshopify.com", "");
+    const appHandle = "wishifylist"
+    const returnUrl = `https://vital-wish-list-c86fce13cbd9.herokuapp.com/charge_approval`;
 
-const response = await admin.graphql(
-  `#graphql
-  mutation AppSubscriptionCreate($name: String!, $lineItems: [AppSubscriptionLineItemInput!]!, $returnUrl: URL!) {
-    appSubscriptionCreate(name: $name, returnUrl: $returnUrl, lineItems: $lineItems) {
-      userErrors {
-        field
-        message
-      }
-      appSubscription {
-        id
-      }
-      confirmationUrl
-    }
-  }`,
-  {
-    variables: {
-      "name": "Wishify Access",
-      "returnUrl": returnUrl,
-      "lineItems": [
-        {
-          "plan": {
-            "appRecurringPricingDetails": {
-              "price": {
-                "amount": 5.00,
-                "currencyCode": "USD"
-              },
-              "interval": "EVERY_30_DAYS"
-            }
-          }
+    const response = await admin.graphql(
+    `#graphql
+    mutation AppSubscriptionCreate($name: String!, $lineItems: [AppSubscriptionLineItemInput!]!, $returnUrl: URL!) {
+        appSubscriptionCreate(name: $name, returnUrl: $returnUrl, lineItems: $lineItems) {
+        userErrors {
+            field
+            message
         }
-      ]
+        appSubscription {
+            id
+        }
+        confirmationUrl
+        }
+    }`,
+    {
+        variables: {
+        "name": "Wishify Access",
+        "returnUrl": returnUrl,
+        "lineItems": [
+            {
+            "plan": {
+                "appRecurringPricingDetails": {
+                "price": {
+                    "amount": 5.00,
+                    "currencyCode": "USD"
+                },
+                "interval": "EVERY_30_DAYS"
+                }
+            }
+            }
+        ]
+        },
     },
-  },
-);
+    );
 
-const data = await response.json();
+    const data = await response.json();
+    return data
+}
+
+handleSubscription()
