f7bb3afe5f94dedabead920e5cedeb474a893b2b
CarlosCardenas24 <nooptions101@gmail.com>
2024-10-16 14:38:15 -0700

    added subscription.jsx for managed pricing, still needs work to be done

---
+++ app/routes/subscription.jsx
@@ -0,0 +1,42 @@
+const { session, admin } = await authenticate.admin(request);
+
+const shop = session.shop.replace(".myshopify.com", "");
+const appHandle = "wishifylist"
+const returnUrl = `https://admin.shopify.com/store/${shop}/apps/${appHandle}/charge_approval`;
+
+const response = await admin.graphql(
+  `#graphql
+  mutation AppSubscriptionCreate($name: String!, $lineItems: [AppSubscriptionLineItemInput!]!, $returnUrl: URL!) {
+    appSubscriptionCreate(name: $name, returnUrl: $returnUrl, lineItems: $lineItems) {
+      userErrors {
+        field
+        message
+      }
+      appSubscription {
+        id
+      }
+      confirmationUrl
+    }
+  }`,
+  {
+    variables: {
+      "name": "Wishify Access",
+      "returnUrl": returnUrl,
+      "lineItems": [
+        {
+          "plan": {
+            "appRecurringPricingDetails": {
+              "price": {
+                "amount": 5.00,
+                "currencyCode": "USD"
+              },
+              "interval": "EVERY_30_DAYS"
+            }
+          }
+        }
+      ]
+    },
+  },
+);
+
+const data = await response.json();
