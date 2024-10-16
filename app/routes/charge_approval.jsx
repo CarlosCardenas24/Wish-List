b24f104beb47f9270987c0db52ab79eba9f69627
CarlosCardenas24 <nooptions101@gmail.com>
2024-10-16 14:45:05 -0700

    added charge_approval.jsx, testing to see if it does render

---
+++ app/routes/charge_approval.jsx
@@ -0,0 +1,9 @@
+export default function ChargeApproval() {
+    return (
+      <div>
+        <h1>Charge Approval</h1>
+        <p>Your subscription charge has been successfully approved.</p>
+      </div>
+    );
+  }
+  
