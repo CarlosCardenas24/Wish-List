b206c1aff0e3b67bc7c6a155dd3e1060421d5c53
CarlosCardenas24 <nooptions101@gmail.com>
2024-11-22 23:48:30 -0800

    removed a div layer

--- app/routes/app.instructions.jsx
+++ app/routes/app.instructions.jsx
@@ -44,29 +44,27 @@ return (
                                 <Text as="p" variant="bodyMd">
                                     The app blocks should work on all Online Store 2.0 Themes. Here is a video on how to set it up.
                                 </Text>
-                                <div style={{ display: 'flex', justifyContent: 'center' }}>
-                                    <div style={{position: 'relative',
-                                    paddingBottom: '56.25%', // 16:9 aspect ratio
-                                    height: 0,
-                                    width: '100%',
-                                    maxWidth: '560px',
-                                    }}>
-                                        <iframe
-                                        style={{
-                                            position: 'absolute',
-                                            top: 0,
-                                            left: 0,
-                                            width: '100%',
-                                            height: '100%',
-                                        }}
-                                        src="https://www.youtube.com/embed/rbeSYHIIwzE?si=SBZpi94jWD5zJaW5"
-                                        title="YouTube video player"
-                                        frameBorder="0"
-                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                        referrerPolicy="strict-origin-when-cross-origin"
-                                        allowFullScreen
-                                        ></iframe>
-                                    </div>
+                                <div style={{position: 'relative',
+                                paddingBottom: '56.25%', // 16:9 aspect ratio
+                                height: 0,
+                                width: '100%',
+                                maxWidth: '560px',
+                                }}>
+                                    <iframe
+                                    style={{
+                                        position: 'absolute',
+                                        top: 0,
+                                        left: 0,
+                                        width: '100%',
+                                        height: '100%',
+                                    }}
+                                    src="https://www.youtube.com/embed/rbeSYHIIwzE?si=SBZpi94jWD5zJaW5"
+                                    title="YouTube video player"
+                                    frameBorder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                    referrerPolicy="strict-origin-when-cross-origin"
+                                    allowFullScreen
+                                    ></iframe>
                                 </div>
                             </>
                             )}
@@ -87,29 +85,27 @@ return (
                                     <Text as="p" variant="bodyMd">
                                         While these embedded blocks should work on any theme, you will need the Online Store 2.0 themes for the app blocks. Here is a video on how to turn them on
                                     </Text>
-                                    <div style={{ display: 'flex', justifyContent: 'center' }}>
-                                        <div style={{position: 'relative',
-                                        paddingBottom: '56.25%', // 16:9 aspect ratio
-                                        height: 0,
-                                        width: '100%',
-                                        maxWidth: '560px',
-                                        }}>
-                                            <iframe
-                                            style={{
-                                                position: 'absolute',
-                                                top: 0,
-                                                left: 0,
-                                                width: '100%',
-                                                height: '100%',
-                                            }}
-                                            src="https://www.youtube.com/embed/20fVsSS2aWU?si=nSb6rx2mwDY-WT5V"
-                                            title="YouTube video player"
-                                            frameBorder="0"
-                                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                            referrerPolicy="strict-origin-when-cross-origin"
-                                            allowFullScreen
-                                            ></iframe>
-                                        </div>
+                                    <div style={{position: 'relative',
+                                    paddingBottom: '56.25%', // 16:9 aspect ratio
+                                    height: 0,
+                                    width: '100%',
+                                    maxWidth: '560px',
+                                    }}>
+                                        <iframe
+                                        style={{
+                                            position: 'absolute',
+                                            top: 0,
+                                            left: 0,
+                                            width: '100%',
+                                            height: '100%',
+                                        }}
+                                        src="https://www.youtube.com/embed/20fVsSS2aWU?si=nSb6rx2mwDY-WT5V"
+                                        title="YouTube video player"
+                                        frameBorder="0"
+                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                        referrerPolicy="strict-origin-when-cross-origin"
+                                        allowFullScreen
+                                        ></iframe>
                                     </div>
                                 </>
                             </div>
